fb2fb70db0462e265e1a4fee79141088a369d460
RyanByers1023 <113501683+RyanByers1023@users.noreply.github.com>
2024-03-20 13:18:35 -0500

    Update main.cpp
    commented logic regarding the future development of an important screen method, deltaTime.

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -18,6 +18,12 @@ int SDL_main(int arg, char* args[]) {
 		screen.Clear(); //clear the screen (will also clear out screen.vertices)
 		screen.CheckForInput(); //check for user input
 		SDL_Delay(15); //this will determine the frame rate of the simulation. set to update every 15 msecs. lower value == higher framerate. Higher framerate = faster simulation speed. I will need to solve this problem later.
+		//solution to this problem: need time since last update (store as msec since last update as an int, deltaTime) better as float?? i dont know
+		//any changes made using frame changes as a unit of time need to be multiplied by deltaTime (i think this is commonly referred to as normalization)
+		//possible that it may be efficient to store this value, deltaTime, in screen as an attribute eg. screen.deltaTime.
+		//reasoning: screen is passed to mostly everything as this is a required parameter of most if not all of the SDL2 methods i am using, so I may as well store it here if I need such constant access to it
+		//this (deltaTime) will have the effect of making both physics calculations and any transformations, rotations, etc. not change their speed when framerate increases (increase in speed) or when the framerate decreases (decrease in speed)
+		//the framerate will fluctuate depending on the hardware used to run the simulation. dont want this. deltaTime should solve this dilemma.
 	}
 	return 0;
 }
