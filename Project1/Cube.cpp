72f3b1fc7d2616d5ec6b753a79f79c3002d528a4
Ryan Byers <ryanbyers1023@gmail.com>
2024-03-28 15:01:57 -0500

    Remove many methods from Cube.cpp. Add to PrimitiveObject and LinearTransformations

--- Project1/Cube.cpp
+++ Project1/Cube.cpp
@@ -1,22 +1,21 @@
 #include "Cube.h"
 
-Cube::Cube(Screen& screen, vec3 position, float size) : Cube(){ //default dimensions: 1px x 1px x 1px
+Cube::Cube(Screen& screen, vec3 position, float size){ //Default dimensions: 1px x 1px x 1px. Default position = {0, 0, 0} - origin. Below pretend values are constructed with these inputs in mind as an example
 	this->screenPtr = &screen;
 	this->position = position;
 
 	// Definition of the vertices of the cube
-	vertices[0] = { position.x, position.y, position.z };                        // (0, 0, 0)
-	vertices[1] = { position.x + size, position.y, position.z };                 // (1, 0, 0)
-	vertices[2] = { position.x + size, position.y + size, position.z };          // (1, 1, 0)
-	vertices[3] = { position.x, position.y + size, position.z };                 // (0, 1, 0)
-	vertices[4] = { position.x, position.y, position.z + size };                 // (0, 0, 1)
-	vertices[5] = { position.x + size, position.y, position.z + size };          // (1, 0, 1)
-	vertices[6] = { position.x + size, position.y + size, position.z + size };   // (1, 1, 1)
-	vertices[7] = { position.x, position.y + size, position.z + size }; ;		 // (0, 1, 1)
+	vertices.push_back({ position.x, position.y, position.z });                        // (0, 0, 0) --- [0]
+	vertices.push_back({ position.x + size, position.y, position.z });                 // (1, 0, 0) --- [1]
+	vertices.push_back({ position.x + size, position.y + size, position.z });          // (1, 1, 0) --- [2]
+	vertices.push_back({ position.x, position.y + size, position.z });                 // (0, 1, 0) --- [3] ----> 8 total vertices
+	vertices.push_back({ position.x, position.y, position.z + size });                 // (0, 0, 1) --- [4]
+	vertices.push_back({ position.x + size, position.y, position.z + size });          // (1, 0, 1) --- [5]
+	vertices.push_back({ position.x + size, position.y + size, position.z + size });   // (1, 1, 1) --- [6]
+	vertices.push_back({ position.x, position.y + size, position.z + size }); ;		   // (0, 1, 1) --- [7]
 
 	//use the above vertices of the cube to make the cube mesh
-	//built in a counterclockwise fashion
-	cubeMesh.triangles = {
+	primitiveMesh.triangles = {
 		// SOUTH FACE
 		{vertices[0], vertices[3], vertices[2]}, //{vec3, vec3, vec3} ---> {(x, y, z), (x, y, z), (x, y, z)}
 		{vertices[0], vertices[2], vertices[1]},
--- Project1/Cube.h
+++ Project1/Cube.h
@@ -3,11 +3,6 @@
 
 class Cube:public PrimitiveObject {
 public:
-	//constructor (requires, at the least, the screen variable)
+	//constructor
 	Cube(Screen& screen, vec3 position = {0.0f, 0.0f, 0.0f}, float size = 1); //forms the mesh of the cube, retrieves screenPtr, and gives the postion at which to spawn the cube in
-	Cube() : vertices(8) {}; //initilization constructor that initializes the vertices vector with a size of 8. runs when the above constructor runs
-private:
-	//private attributes
-	mesh cubeMesh; //the mesh of the cube is stored here. Made up entirely of triangle objects. Triangle objects store 3 coordinates in 3 space. these are stored as vec3s. vec3s are float(x, y, z)
-	std::vector<vec3> vertices; //initialized with a size of 8
 };
