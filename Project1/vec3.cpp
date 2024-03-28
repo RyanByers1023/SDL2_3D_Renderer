07c631543784add5239ed3401ebdf476be0e0ea5
Ryan Byers <ryanbyers1023@gmail.com>
2024-03-28 15:04:04 -0500

    Add operator overload for *= for Matrix4x4 multiplication

--- Project1/vec3.cpp
+++ Project1/vec3.cpp
@@ -1,10 +1,50 @@
 #include "vec3.h"
-#include <vector>
 
 vec3::vec3() {
 	this->x = 0.0f;
 	this->y = 0.0f;
 	this->z = 0.0f;
+}
+
+vec3& vec3::operator*=(const Matrix4x4& transformationMatrix) {
+    // Perform the transformation in place
+    float newX = x * transformationMatrix.matrix[0][0] + y * transformationMatrix.matrix[1][0] + z * transformationMatrix.matrix[2][0] + transformationMatrix.matrix[3][0];
+    float newY = x * transformationMatrix.matrix[0][1] + y * transformationMatrix.matrix[1][1] + z * transformationMatrix.matrix[2][1] + transformationMatrix.matrix[3][1];
+    float newZ = x * transformationMatrix.matrix[0][2] + y * transformationMatrix.matrix[1][2] + z * transformationMatrix.matrix[2][2] + transformationMatrix.matrix[3][2];
+
+    float w = x * transformationMatrix.matrix[0][3] + y * transformationMatrix.matrix[1][3] + z * transformationMatrix.matrix[2][3] + transformationMatrix.matrix[3][3];
+
+    // If w is not zero, convert to Cartesian coordinates
+    if (w != 0.0f) {
+        newX /= w;
+        newY /= w;
+        newZ /= w;
+    }
+
+    // Update the components of the vector
+    x = newX;
+    y = newY;
+    z = newZ;
+
+    return *this;
+}
+
+vec3 vec3::operator*(const Matrix4x4& transformationMatrix) const {
+    vec3 outputVector;
+
+    outputVector.x = x * transformationMatrix.matrix[0][0] + y * transformationMatrix.matrix[1][0] + z * transformationMatrix.matrix[2][0] + transformationMatrix.matrix[3][0];
+    outputVector.y = x * transformationMatrix.matrix[0][1] + y * transformationMatrix.matrix[1][1] + z * transformationMatrix.matrix[2][1] + transformationMatrix.matrix[3][1];
+    outputVector.z = x * transformationMatrix.matrix[0][2] + y * transformationMatrix.matrix[1][2] + z * transformationMatrix.matrix[2][2] + transformationMatrix.matrix[3][2];
+
+    float w = x * transformationMatrix.matrix[0][3] + y * transformationMatrix.matrix[1][3] + z * transformationMatrix.matrix[2][3] + transformationMatrix.matrix[3][3];
+
+    if (w != 0.0f) {
+        outputVector.x /= w;
+        outputVector.y /= w;
+        outputVector.z /= w;
+    }
+
+    return outputVector;
 }
 
 vec3::vec3(float x, float y, float z) {
@@ -17,20 +57,4 @@ vec3::vec3(int x, int y, int z) {
 	this->x = static_cast<float>(x);
 	this->y = static_cast<float>(y);
 	this->z = static_cast<float>(z);
-}
-
-void vec3::Rotate(vec3 rotationVector) { //values x, y, and z must = desired rotations in radians.
-	float rad = 0;
-
-	rad = rotationVector.x;
-	this->y = cos(rad) * this->y - sin(rad) * this->z;
-	this->z = sin(rad) * this->y + cos(rad) * this->z;
-
-	rad = rotationVector.y;
-	this->x = cos(rad) * this->x + sin(rad) * this->z;
-	this->z = -sin(rad) * this->x + cos(rad) * this->z;
-
-	rad = rotationVector.z;
-	this->x = cos(rad) * this->x - sin(rad) * this->y;
-	this->y = sin(rad) * this->x + cos(rad) * this->y;
 }
--- Project1/vec3.h
+++ Project1/vec3.h
@@ -1,6 +1,6 @@
 #pragma once
-#include <cmath>
 #include <vector>
+#include "Basic4x4Matrices.h"
 
 class vec3 { //a point (represented by floats) in 3-Space (x, y, z)
 public:
@@ -8,12 +8,13 @@ public:
 	vec3();
 	vec3(float x, float y, float z);
 	vec3(int x, int y, int z);
+	//overloaders
+	vec3& operator*=(const Matrix4x4& transformationMatrix);
+	vec3 operator*(const Matrix4x4& transformationMatrix) const;
 	//public attributes
 	float x;
 	float y;
 	float z;
-	//public methods
-	void Rotate(vec3 rotationVector);
 };
 
 struct triangle { //triangle = 3 x vec3 --> {(x, y, z), (x, y, z), (x, y, z)}
