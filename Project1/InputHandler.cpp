29b9554b57b6b3ae86594814c26080037d622174
Ryan Byers <ryanbyers1023@gmail.com>
2024-03-29 19:38:28 -0500

    Create InputHandler. Handles all user input. Taken from screen.cpp and screen.h in an effort to refactor and improve code readability/modularity.

--- Project1/InputHandler.cpp
+++ Project1/InputHandler.cpp
@@ -1,2 +1,82 @@
 #include "InputHandler.h"
 
+void InputHandler::CheckForInput() { 
+	while (SDL_PollEvent(&event)) {
+		if (event.type == SDL_QUIT) {
+			SDL_Quit();
+			exit(0);
+		}
+		if (event.type == SDL_KEYDOWN) {
+			switch (event.key.keysym.sym) {
+			case SDLK_LEFT:
+				leftInput = true;
+				break;
+			case SDLK_RIGHT:
+				rightInput = true;
+				break;
+			case SDLK_UP:
+				upInput = true;
+				break;
+			case SDLK_DOWN:
+				downInput = true;
+				break;
+
+			case SDLK_w:
+				wInput = true;
+				break;
+			case SDLK_a:
+				aInput = true;
+				break;
+			case SDLK_s:
+				sInput = true;
+				break;
+			case SDLK_d:
+				dInput = true;
+				break;
+			case SDLK_z:
+				zInput = true;
+				break;
+			case SDLK_x:
+				xInput = true;
+				break;
+			}
+
+
+		}
+		if (event.type == SDL_KEYUP) {
+			switch (event.key.keysym.sym) {
+			case SDLK_LEFT:
+				leftInput = false;
+				break;
+			case SDLK_RIGHT:
+				rightInput = false;
+				break;
+			case SDLK_UP:
+				upInput = false;
+				break;
+			case SDLK_DOWN:
+				downInput = false;
+				break;
+
+			case SDLK_w:
+				wInput = false;
+				break;
+			case SDLK_a:
+				aInput = false;
+				break;
+			case SDLK_s:
+				sInput = false;
+				break;
+			case SDLK_d:
+				dInput = false;
+				break;
+			case SDLK_z:
+				zInput = false;
+				break;
+			case SDLK_x:
+				xInput = false;
+				break;
+			}
+		}
+	}
+}
--- Project1/InputHandler.h
+++ Project1/InputHandler.h
@@ -1,1 +1,24 @@
 #pragma once
+#include "SDL.h"
+
+class InputHandler {
+public:
+	//public methods:
+	void CheckForInput();
+	//public input flags:
+	bool leftInput = false;
+	bool rightInput = false;
+	bool upInput = false;
+	bool downInput = false;
+
+	bool wInput = false;
+	bool aInput = false;
+	bool sInput = false;
+	bool dInput = false;
+	bool zInput = false;
+	bool xInput = false;
+
+private:
+	//private attributes:
+	SDL_Event event;
+};
