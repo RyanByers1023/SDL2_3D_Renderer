2f5b37a6cf9184f01580371444df8bcadaacf15a
Ryan Byers <ryanbyers1023@gmail.com>
2024-04-05 00:44:54 -0500

    Change Spawner's SpawnCube arguments: takes in WorldObjects instead of renderer to create objects.

--- Project1/Spawner.cpp
+++ Project1/Spawner.cpp
@@ -1,11 +1,11 @@
 #include "Spawner.h"
 
-void SpawnCube(Renderer& renderer, std::string name, vec3 position, int size) {
+void SpawnCube(WorldObjects* worldObjectsPtr, std::string name, vec3 position, int size) {
 	Cube cube(position, size); //create a 100px x 100px x 100px cube in the center of the screen. parameters: (Screen object, position to spawn object (vec3), size of cube (int in pixels)
-	auto it = renderer.worldObjects.find(name);
-	if (it != renderer.worldObjects.end()) {
+	auto it = worldObjectsPtr->objects.find(name);
+	if (it != worldObjectsPtr->objects.end()) {
 		std::cout << "There already exists an object with that name. Please name the object you wish to create something different.\n\n" << std::endl;
 		return;
 	}
-	renderer.worldObjects[name] = cube;
+	worldObjectsPtr->objects[name] = cube;
 }
--- Project1/Spawner.h
+++ Project1/Spawner.h
@@ -1,9 +1,7 @@
 #pragma once
-#include <vector>
-#include <string>
-#include "PrimitiveObject.h"
 #include "Cube.h"
-#include "Screen.h"
-#include "Renderer.h"
+#include "WorldObjects.h"
+#include <string>
+#include <iostream>
 
-void SpawnCube(Renderer& renderer, std::string name, vec3 position = {40.0f, 20.0f, 90.0f}, int size = 100);
+void SpawnCube(WorldObjects* worldObjectsPtr, std::string name, vec3 position = {40.0f, 20.0f, 90.0f}, int size = 100);
