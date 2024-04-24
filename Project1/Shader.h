0c4f317f2ae225c9cb501f80661dc6966eed7010
RyanByers1023 <ryanbyers1023@gmail.com>
2024-04-24 17:38:03 -0500

    Add: Function declarations for shader helper functions. Move: BoundingBox struct definition to Shader.h

--- Project1/Shader.h
+++ Project1/Shader.h
@@ -1,14 +1,20 @@
 #pragma once
 #include "Screen.h"
 #include "Vec2.h"
-#include "Line.h"
+
+struct BoundingBox{ //used for determing the area we need to check for intersections
+    Vec2 minPoint; // -- > top-left corner of bounding box
+    Vec2 maxPoint; // -- > bottom-right corner of bounding box
+}
 
 class Shader {
 public:
 	Shader(Screen* screenPtr);
 	~Shader();
-	void FillTriangle(const Triangle2D& projTriangle);
+	void ShadeTriangle(const Triangle2D& projTriangle);
 private:
+	BoundingBox GetBoundingBox(const Triangle2d& projTriangle);
+	bool IsInsideTriangle(const Vec2& pointToRender, const Triangle2D& projTriangle);
+	float GetEdgeFunctionValue(const Vec2& pointToRender, const Vec2& v0, const Vec2& v1);
 	Screen* screenPtr;
-	Line* linePtr;
 };
