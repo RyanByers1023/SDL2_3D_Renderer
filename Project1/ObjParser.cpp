8ff0ba1f76f39fa63db7c02fe0599c993eb66e8d
RyanByers1023 <ryanbyers1023@gmail.com>
2024-05-04 20:22:12 -0500

    Add: fuctions to parse obj file. (INCOMPLETE)

--- Project1/ObjParser.cpp
+++ Project1/ObjParser.cpp
@@ -1,13 +1,47 @@
 #include "ObjParser.h"
 
-bool OpenObjFile(string filename){ //this parser assumes the obj file only contains vertex information
+//this will be the main function that returns the entire comepleted mesh
+primitiveMesh GetMesh(){
+
+}
+
+bool OpenObjFile(string filename){ //this parser assumes the obj file only contains vertex information (will crash otherwise, i should probably handle this)
     std::ifstream objFile (filename);
-    if(!objFile.is_open()){
-        std::cerr<< "Failed to open file. Did you enter the correct objFile name?"<< std::endl;
+
+    if(objFile.is_open()){ //file was found in program main directory
+        GetLine()
+    }
+    else{ //file was not found in program main directory
+        std::cerr<< "Failed to open object file. File not found. Did you enter the correct .obj File name?"<< std::endl;
+        return false;
+    }    
+    return true;
+}
+
+bool GetLines(const std::ifstream& objFile, string& line){
+    if(getline(objFile, line)){
+        return true;
+    }
+    else{
         return false;
     }
-    std::istringstream
-    
-    
-    return true;
+}
+
+Vec3 GetVertex(std::string line){ 
+    char vertexCheck;
+
+    while(GetLines()){ //the current line from objFile is now = to line
+            std::istringstream vertexLine(line);
+            vertexLine >> vertexCheck;
+            if(vertexCheck == 'v'){ //this is a line that is going to give me vertex information
+                float x, y, z;
+                vertexLine >> x >> y >> z;
+                //create a new vec3 object and intialize with these values
+                Vec3 vertex;
+                vertex.x = x;
+                vertex.y = y;
+                vertex.z = z;
+                //we now have extracted a vertex from the obj file that is to be a part of the mesh
+            }
+    }
 }
