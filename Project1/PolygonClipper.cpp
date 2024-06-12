efcd0fa1617571de2f143ccc2724679cbfae654f
RyanByers1023 <ryanbyers1023@gmail.com>
2024-06-12 14:36:32 -0500

    Add code to ClipVertices()

--- Project1/PolygonClipper.cpp
+++ Project1/PolygonClipper.cpp
@@ -28,56 +28,61 @@
     //5. save intersections in new list respective to four above relationships
     //6. repeat steps 4 and 5 for all edges of clipping window
 
-float GetEdgeFunctionDet(const Vec2& v1, const Vec2& v2, const Vec2& currVertex){ //Computes cross product between edge v1 -> v2 with respect to currVertex
-    //Eqn: P = (x2 - x1) * (y - y1) - (y2 -y1) * (x - x1)
-    return (v2.x - v1.x) * (currVertex.y - v1.y) - (v2.y - v1.y) * (currVertex.x - v1.x);
-}
-
-//if clipper vertices are defined counter-clockwise (they will be), then all points to the LEFT are inside the clipper boundaries
-bool VertexInside(float determinant, const Vec2& v1, const Vec2& v2, const Vec2& currVertex){
-    return GetEdgeFunctionDet(v1, v2, currVertex) >= 0;
-}
-
-//assume v1 and v2 make up the edge of the clipping window
-std::vector<Vec2> ClipVertices(const Vec2& v1, const Vec2& v2, const Triangle2D& triToClip){
-    std::vector<Vec2> newVertices;
+std::vector<Vec2> ClipVertices(const Edge& clipWindowEdge, const Triangle2D& triToClip){ //return a list of vertices that should have an edge rendered between them
+    std::vector<Vec2> newVertices; //used to store the clipped vertices --> will return this list
+    Vec2 intercept; //used to store intercept between triangle edge and clipWindowEdge
+    Edge triEdge; //stores current edge this function is trying to clip
     for(int i = 0; i < triToClip.vertices.size(); ++i){
-        if(i + 1 == 3) triVertex2 = triToClip.vertices[0];
-        else triVertex2 = triToClip.vertices[i + 1];
+        //set up vertices vi -> vi+1 that make up the edge I want to check for
+        triEdge.v1 = triToClip.vertices[i];
+        triEdge.v2 = (i + 1 == 3) ? vertices[0] : vertices[i + 1]; //v[0] on last iteration v1 -> v3
+
         //Both inside -- keep second vertex only
-        if(VertexInside(v1, v2, triVertex1) && VertexInside(v1, v2, triVertex2)){
+        if(VertexInside(clipWindowEdge, triEdge.v1) && VertexInside(clipWindowEdge, triEdge.v2)){
             newVertices.push_back(triVertex.vertices[i + 1]);
         }
         //First inside, second outside -- keep only point of intersection w/ first point
-        else if(VertexInside(v1, v2, triVertex1) && !VertexInside(v1, v2, triVertex2)){
-            Vec2 intercept = {FindXIntercept(v1, v2, triVertex1), FindYIntercept(v1, v2, triVertex2)};
+        else if(VertexInside(clipWindowEdge, triEdge.v1) && !VertexInside(clipWindowEdge, triEdge.v2)){
+            intercept = FindIntercept(clipWindowEdge, triEdge);
             newVertices.push_back(intercept);
         }
         //First outside, second inside -- keep only point of intersection w/ second point
-        else if(!VertexInside(v1, v2, triVertex1) && VertexInside(v1, v2, triVertex2)){
-            Vec2 intercept = {FindXIntercept(v1, v2, triVertex2), FindYIntercept(v1, v2, triVertex2)};
+        else if(!VertexInside(clipWindowEdge, triEdge.v1) && VertexInside(clipWindowEdge, triEdge.v2)){
+            intercept = {FindXIntercept(clipWindowEdge, triEdge.v2), FindYIntercept(clipWindowEdge, triEdge.v2)};
             newVertices.push_back(intercept);
         }
         //If none of these are eval. to true, then the verts are both outside -- do not keep any vertices
 
         return newVertices;
-    }
-    
+    }   
+}
+
+float GetEdgeFunctionDet(const Edge& clipWindowEdge, const Vec2& currVertex){ //Computes cross product between edge v1 -> v2 with respect to currVertex
+    //Eqn: P = (x2 - x1) * (y - y1) - (y2 -y1) * (x - x1)
+    Vec2 v1 = clipWindowEdge.v1, v2 = clipWindowEdge.v2;
+    return (v2.x - v1.x) * (currVertex.y - v1.y) - (v2.y - v1.y) * (currVertex.x - v1.x);
+}
+
+//if clipper vertices are defined counter-clockwise (they will be), then all points to the LEFT are inside the clipper boundaries
+bool VertexInside(float determinant, const Vec2& v1, const Vec2& v2, const Vec2& currVertex){
+    return GetEdgeFunctionDet(clipWindowEdge, currVertex) >= 0;
 }
 
 //Assume clip boundary = v1 -> v2 and polygon edge = v3 -> v4
 //v1 = (x1, y1), v2 = (x2, y2), v3 = (x3, y3), v4 = (x4, y4) 
 
-float FindXIntercept(const Vec2& v1, const Vec2& v2, const Vec2& v3, const Vec2& v4){
+Vec2 FindIntercept(const Edge& clipWindowEdge, const Edge& triangleEdge){
+    Vec2 v1 = clipWindowEdge.v1, v2 = clipWindowEdge.v2, v3 = triangleEdge.v1, v4 = triangleEdge.v2;
+
+    Vec2 output;
+
     //x-intercept = ((x1y2 - y1x2)(x3 - x4) - (x1 - x2)(x3y4 - y3x4)) / ((x1 - x2)(y3 - y4) - (y1 - y2)(x3 - x4))
+    output.x = ((v1.x * v2.y) - (v1.y * v2.x) * (v3.x - v4.x) - (v1.x - v2.x) * (v3.x * v4.y) - (v3.y * v4.x)) / 
+               ((v1.x - v2.x) * (v3.y - v4.y) - (v1.y - v2.y) * (v3.x - v4.x));
 
-    return ((v1.x * v2.y) - (v1.y * v2.x) * (v3.x - v4.x) - (v1.x - v2.x) * (v3.x * v4.y) - (v3.y * v4.x)) / 
-           ((v1.x - v2.x) * (v3.y - v4.y) - (v1.y - v2.y) * (v3.x - v4.x));
-}
-
-float FindYIntercept(){
     //y-intercept = ((x1y2 - y1x2)(y3 -y4) - (y1 - y2)(x3y4 - y3x4)) / ((x1 - x2)(y3 - y4) - (y1 - y2)(x3 -x4))
+    output.y = ((v1.x * v2.y) - (v1.y * v2.x) * (v3.y - v4.y) - (v1.y - v2.y) * (v3.x * v4.y) - (v3.y * v4.x))/
+               ((v1.x - v2.x) * (v3.y - v4.y) - (v1.y - v2.y) * (v3.x - v4.x));
 
-    return ((v1.x * v2.y) - (v1.y * v2.x) * (v3.y - v4.y) - (v1.y - v2.y) * (v3.x * v4.y) - (v3.y * v4.x))/
-           ((v1.x - v2.x) * (v3.y - v4.y) - (v1.y - v2.y) * (v3.x - v4.x));
+    return output;
 }
