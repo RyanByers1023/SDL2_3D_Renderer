52d4016c14ca238d65762b1ebe448415dfce7ae4
Ryan Byers <ryanbyers1023@gmail.com>
2024-06-18 15:32:40 -0500

    Remove declarations for bounding box related clipping functions. To be replace with functions in PolygonClipper

--- Project1/Shader.h
+++ Project1/Shader.h
@@ -3,19 +3,15 @@
 #include "Vec2.h"
 #include <cmath>
 
-struct BoundingBox{ //used for determing the area we need to check for intersections
-    Vec2 minPoint; // -- > top-left corner of bounding box
-    Vec2 maxPoint; // -- > bottom-right corner of bounding box
-}
+struct BoundingBox { //used for determing the area we need to check for intersections
+	Vec2 minPoint; // -- > top-left corner of bounding box
+	Vec2 maxPoint; // -- > bottom-right corner of bounding box
+};
 
 class Shader {
 public:
 	Shader(Screen* screenPtr);
 	void ShadePolygon(const Polygon2D& polygon);
 private:
-	BoundingBox GetBoundingBox(const Triangle2d& projTriangle);
-	void ClampBoundingBox(BoundingBox& boundingBox);
-	bool IntersectsTriangle(const Vec2& pointToCheck, const Triangle2D& projTriangle);
-	float GetEdgeFunctionDet(const Vec2& pointToRender, const Vec2& v0, const Vec2& v1);
 	Screen* screenPtr;
 };
