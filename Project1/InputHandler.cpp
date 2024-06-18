cfef54b31f554eb4d98ae8fbdee28b7dff0826f5
Ryan Byers <ryanbyers1023@gmail.com>
2024-06-18 15:26:40 -0500

    Fix syntax error regarding curly bracket misallignment

--- Project1/InputHandler.cpp
+++ Project1/InputHandler.cpp
@@ -13,108 +13,108 @@ void InputHandler::CheckForInput() {
 			HandleKeyUp(event);
 		}
 	}
+}
 
-	void InputHandler::HandleKeyDown(SDL_Event event){
-		switch (event.key.keysym.sym) { //switch used to determine which key has been pressed
-			//arrow keys
-			case SDLK_LEFT:
-				leftInput = true;
-				break;
-			case SDLK_RIGHT:
-				rightInput = true;
-				break;
-			case SDLK_UP:
-				upInput = true;
-				break;
-			case SDLK_DOWN:
-				downInput = true;
-				break;
+void InputHandler::HandleKeyDown(SDL_Event event) {
+	switch (event.key.keysym.sym) { //switch used to determine which key has been pressed
+		//arrow keys
+	case SDLK_LEFT:
+		leftInput = true;
+		break;
+	case SDLK_RIGHT:
+		rightInput = true;
+		break;
+	case SDLK_UP:
+		upInput = true;
+		break;
+	case SDLK_DOWN:
+		downInput = true;
+		break;
 
-			//WASD keys (XY plane movement)
-			case SDLK_w:
-				wInput = true;
-				break;
-			case SDLK_a:
-				aInput = true;
-				break;
-			case SDLK_s:
-				sInput = true;
-				break;
-			case SDLK_d:
-				dInput = true;
-				break;
-			
-			//Rotation keys Z/X
-			case SDLK_z:
-				zInput = true;
-				break;
-			case SDLK_x:
-				xInput = true;
-				break;
+		//WASD keys (XY plane movement)
+	case SDLK_w:
+		wInput = true;
+		break;
+	case SDLK_a:
+		aInput = true;
+		break;
+	case SDLK_s:
+		sInput = true;
+		break;
+	case SDLK_d:
+		dInput = true;
+		break;
 
-			//Scaling keys Q/E
-			case SDLK_q:
-				qInput = true;
-				break;
-			case SDLK_e:
-				eInput = true;
-				break;
+		//Rotation keys Z/X
+	case SDLK_z:
+		zInput = true;
+		break;
+	case SDLK_x:
+		xInput = true;
+		break;
 
-			//+/- keys Z plane movement
-			case SDLK_MINUS:
-				minusInput = true;
-				break;
-			case SDLK_PLUS:
-				plusInput = true;
-				break;
-		}
+		//Scaling keys Q/E
+	case SDLK_q:
+		qInput = true;
+		break;
+	case SDLK_e:
+		eInput = true;
+		break;
 
-		void InputHandler::HandleKeyUp(SDL_Event event){
-			switch (event.key.keysym.sym) { //switch used to determine which key was undepressed
-			case SDLK_LEFT:
-				leftInput = false;
-				break;
-			case SDLK_RIGHT:
-				rightInput = false;
-				break;
-			case SDLK_UP:
-				upInput = false;
-				break;
-			case SDLK_DOWN:
-				downInput = false;
-				break;
+		//+/- keys Z plane movement
+	case SDLK_MINUS:
+		minusInput = true;
+		break;
+	case SDLK_PLUS:
+		plusInput = true;
+		break;
+	}
+}
 
-			case SDLK_w:
-				wInput = false;
-				break;
-			case SDLK_a:
-				aInput = false;
-				break;
-			case SDLK_s:
-				sInput = false;
-				break;
-			case SDLK_d:
-				dInput = false;
-				break;
-			case SDLK_z:
-				zInput = false;
-				break;
-			case SDLK_x:
-				xInput = false;
-				break;
-			case SDLK_q:
-				qInput = false;
-				break;
-			case SDLK_e:
-				eInput = false;
-				break;
-			case SDLK_MINUS:
-				minusInput = false;
-				break;
-			case SDLK_PLUS:
-				plusInput = false;
-				break;
-			}
-		}
+void InputHandler::HandleKeyUp(SDL_Event event){
+	switch (event.key.keysym.sym) { //switch used to determine which key was undepressed
+	case SDLK_LEFT:
+		leftInput = false;
+		break;
+	case SDLK_RIGHT:
+		rightInput = false;
+		break;
+	case SDLK_UP:
+		upInput = false;
+		break;
+	case SDLK_DOWN:
+		downInput = false;
+		break;
+
+	case SDLK_w:
+		wInput = false;
+		break;
+	case SDLK_a:
+		aInput = false;
+		break;
+	case SDLK_s:
+		sInput = false;
+		break;
+	case SDLK_d:
+		dInput = false;
+		break;
+	case SDLK_z:
+		zInput = false;
+		break;
+	case SDLK_x:
+		xInput = false;
+		break;
+	case SDLK_q:
+		qInput = false;
+		break;
+	case SDLK_e:
+		eInput = false;
+		break;
+	case SDLK_MINUS:
+		minusInput = false;
+		break;
+	case SDLK_PLUS:
+		plusInput = false;
+		break;
 	}
 }
