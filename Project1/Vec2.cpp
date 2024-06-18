6bca79c71d113cbf906299b4cc03aa438d5edf27
Ryan Byers <ryanbyers1023@gmail.com>
2024-06-18 15:33:25 -0500

    Clean up .cpp file by utlizing short-hand constructors in .h file

--- Project1/Vec2.cpp
+++ Project1/Vec2.cpp
@@ -1,20 +1,7 @@
 #include "Vec2.h"
 
-Vec2::Vec2() {
-	this->x = 0.0f;
-	this->y = 0.0f;
-}
 
-Vec2::Vec2(float x, float y) {
-	this->x = x;
-	this->y = y;
-}
-
-Vec2::Vec2(int x, int y) {
-	this->x = static_cast<float>(x);
-	this->y = static_cast<float>(y);
-}
-
+//assignment operator to assign from Vec3
 Vec2& Vec2::operator=(const Vec3& otherVector) {
 	x = otherVector.x;
 	y = otherVector.y;
--- Project1/Vec2.h
+++ Project1/Vec2.h
@@ -1,23 +1,33 @@
 #pragma once
 #include "Vec3.h"
+#include <vector>
 
 class Vec2 {
 public:
-	Vec2();
-	Vec2(float x, float y);
-	Vec2(int x, int y);
+	//default constructor
+	Vec2() : x(0.0f), y(0.0f) {};
+
+	//constructor with float parameters
+	Vec2(float x, float y) : x(x), y(y) {};
+
+	//contructor with int parameters
+	Vec2(int x, int y) : x(static_cast<float>(x)), y(static_cast<float>(y)) {};
+
+	//assignment operator to assign from Vec3
 	Vec2& operator=(const Vec3& otherVector);
+
+	//public member variables
 	float x;
 	float y;
 };
 
-struct Edge{
-    Vec2 v1;
-    Vec2 v2;
-}
+struct Edge {
+	Vec2 v1;
+	Vec2 v2;
+};
 
 struct Triangle2D {
-	Vec2 point[3];
+	Vec2 vertices[3];
 };
 
 struct Polygon2D{
