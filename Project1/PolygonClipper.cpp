46d00f9be1ce43d64f76c24ec7f0e5a9aec4c242
Ryan Byers <ryanbyers1023@gmail.com>
2024-08-03 12:02:36 -0500

    Clean up comments. Clip returns Polygon2D. Remove debugging code.

--- Project1/PolygonClipper.cpp
+++ Project1/PolygonClipper.cpp
@@ -1,88 +1,46 @@
 #include "PolygonClipper.h"
-    //Process overview:
-    //the goal is to clip the polygons on the x-axis at screenWidth, and the y-axis at screenHeight.
-    //when clipping is performed, a modification is directly made to the projected 2-D triangle's vertices
-    //these vertices will change only for that frame though...
-    //when the next frame occurs, the 3-D -> 2-D transformation and will overwrite these changes and the process will begin again
-    //Clipping area vertices, assume 1920x1080 resolution: (0, 1080), (1920, 1080), (1920, 0), (0,0)  <--- counter-clockwise order
-
-    //How to determine what side of the window an edge lies?:
-    //Eqn: P = (x2 - x1) * (y - y1) - (y2 -y1) * (x - x1)
-    //Evaluate this using these rules:
-    //P < 0 ---> vertices on right side of line
-    //P = 0 ---> vertices on the line
-    //P > 0 ---> vertices on the left side of the line  
-
-    //---Process to clip edges---
-    //Cases to handle (4)
-    //-Both inside
-    //-First outside, second inside
-    //-First inside, second outside
-    //-Both outside
-
-    //1. get triangle vertices
-    //2. get clipping window (screen space) coordinates
-    //3. pick a clipping window edge to begin clipping with
-    //4. compare vertices of each edge of the triangle with the clipping plane individually
-    //5. save intersections in new list respective to four above relationships
-    //6. repeat steps 4 and 5 for all edges of clipping window
 
 //return a list of vertices that should have an edge rendered between them
-void PolygonClipper::Clip(const Polygon2D& inputPolygon, Polygon2D& newPolygon, const Edge& clipWindowEdge){ 
+Polygon2D PolygonClipper::Clip(const Polygon2D& inputPolygon, const Edge& clipWindowEdge){ 
     Vec2 intercept; //store intercept between triangle edge and clipWindowEdge
     Edge polyEdge; //store current edge this function is trying to clip
+    Polygon2D clippedPolygon; //store clipped edges, will build up throughout 4 (# num of clipEdges) iterations of this function
 
-    //store clipped vertices here temporarily
-    std::vector<Vec2> newVertices;
+    int numvertices = inputPolygon.vertices.size();
 
-    int numVertices = inputPolygon.vertices.size();
-    for(int i = 0; i < numVertices; ++i){ //loop through all edges of triangle (3)
-        //set up vertices vi -> vi+1 that make up the edge I want to check for
+    for(int i = 0; i < numvertices; ++i){ //loop through all edges of polygon
+
+        //obtain vertices, store in polyEdge
         polyEdge.v1 = inputPolygon.vertices[i];
-        polyEdge.v2 = inputPolygon.vertices[(i + 1) % numVertices]; //wrap around to the first vertex on the last iteration to check the last edge
-
-        /*
-        // Print the current edge being processed
-        std::cout << "Processing edge: (" << polyEdge.v1.x << ", " << polyEdge.v1.y << ") to ("
-            << polyEdge.v2.x << ", " << polyEdge.v2.y << ")" << std::endl;
-        */
+        polyEdge.v2 = inputPolygon.vertices[(i + 1) % numvertices]; //wrap around to the first vertex on the last iteration to check the last edge
 
         //Both inside -- keep vertices: second vertex only
         if(VertexInside(clipWindowEdge, polyEdge.v1) && VertexInside(clipWindowEdge, polyEdge.v2)){ //both inside
-            newVertices.push_back(polyEdge.v2); //store second vertex
+            clippedPolygon.vertices.push_back(polyEdge.v2); //store second vertex
         }
 
         //First inside, second outside -- keep vertices: intersection w/ clipWindowEdge
         else if(VertexInside(clipWindowEdge, polyEdge.v1) && !VertexInside(clipWindowEdge, polyEdge.v2)){ //1st inside, 2nd not
             intercept = FindIntercept(clipWindowEdge, polyEdge); //find intercept
 
-            newVertices.push_back(intercept); //store intercept
+            clippedPolygon.vertices.push_back(intercept); //store intercept
         }
 
         //First outside, second inside -- keep vertices: intersection w/ clipWindowEdge + second vertex
         else if(!VertexInside(clipWindowEdge, polyEdge.v1) && VertexInside(clipWindowEdge, polyEdge.v2)){ //1st outside, 2nd inside
             intercept = FindIntercept(clipWindowEdge, polyEdge); //find intercept
             
-            newVertices.push_back(intercept); //store intercept
-            newVertices.push_back(polyEdge.v2); //store second vertex  
+            clippedPolygon.vertices.push_back(intercept); //store intercept
+            clippedPolygon.vertices.push_back(polyEdge.v2); //store second vertex  
         }
         //If none of these are eval. to true, then the verts are both outside -- do not keep any vertices
     }
-
-    /*
-    //Print the vertices of the new polygon
-    std::cout << "New vertices:" << std::endl;
-    for (const Vec2& vertex : newVertices) {
-        std::cout << "(" << vertex.x << ", " << vertex.y << ")" << std::endl;
-    }
-    */
-
-    newPolygon.vertices = newVertices;
+    return clippedPolygon;
 }
 
-//if clipper vertices are defined counter-clockwise (they will be), then all vertices to the RIGHT are inside the clipper boundaries
+//if clipper vertices are defined counter-clockwise (they will be), then all vertices to the LEFT are inside the clipper boundaries
 bool PolygonClipper::VertexInside(const Edge& clipWindowEdge, const Vec2& currVertex) {
-    return GetEdgeFunctionDet(clipWindowEdge, currVertex) <= 0;
+    return GetEdgeFunctionDet(clipWindowEdge, currVertex) <= 0; //note: this currently only returns true when vertices are to the right (edge function >= 0); possible problem with clip window edge order being clockwise instead of counter-clockwise?
 }
 
 float PolygonClipper::GetEdgeFunctionDet(const Edge& clipWindowEdge, const Vec2& currVertex){ //Computes cross product between edge v1 -> v2 with respect to currVertex
@@ -113,4 +71,32 @@ Vec2 PolygonClipper::FindIntercept(const Edge& clipWindowEdge, const Edge& polyE
     //return intercept
     return intercept;
 }
+
+//Process overview:
+//the goal is to clip the polygons on the x-axis at screenWidth, and the y-axis at screenHeight.
+//when clipping is performed, a modification is directly made to the projected 2-D triangle's vertices
+//these vertices will change only for that frame though...
+//when the next frame occurs, the 3-D -> 2-D transformation and will overwrite these changes and the process will begin again
+//Clipping area vertices, assume 1920x1080 resolution: (0, 1080), (1920, 1080), (1920, 0), (0,0)  <--- counter-clockwise order
+
+//How to determine what side of the window an edge lies?:
+//Eqn: P = (x2 - x1) * (y - y1) - (y2 -y1) * (x - x1)
+//Evaluate this using these rules:
+//P < 0 ---> vertices on right side of line
+//P = 0 ---> vertices on the line
+//P > 0 ---> vertices on the left side of the line  
+
+//---Process to clip edges---
+//Cases to handle (4)
+//-Both inside
+//-First outside, second inside
+//-First inside, second outside
+//-Both outside
+
+//1. get triangle vertices
+//2. get clipping window (screen space) coordinates
+//3. pick a clipping window edge to begin clipping with
+//4. compare vertices of each edge of the triangle with the clipping plane individually
+//5. save intersections in new list respective to four above relationships
+//6. repeat steps 4 and 5 for all edges of clipping window
 
