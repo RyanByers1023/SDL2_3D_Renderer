c51fa77b438a7bcefd2920388693eb27c18ae535
RyanByers1023 <ryanbyers1023@gmail.com>
2024-08-03 16:16:32 -0500

    Remove struct: boundingBox. No longer needed due to code within PolygonClipper()

--- Project1/Shader.h
+++ Project1/Shader.h
@@ -3,11 +3,6 @@
 #include "Vec2.h"
 #include <cmath>
 
-struct BoundingBox { //used for determing the area we need to check for intersections
-	Vec2 minPoint; // -- > top-left corner of bounding box
-	Vec2 maxPoint; // -- > bottom-right corner of bounding box
-};
-
 class Shader {
 public:
 	Shader(Screen* screenPtr);
