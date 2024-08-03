6b04448c02cbb60c49f0b318a11599008b440327
Ryan Byers <ryanbyers1023@gmail.com>
2024-08-03 12:03:04 -0500

    Clip returns Polygon2D instead of being void

--- Project1/PolygonClipper.h
+++ Project1/PolygonClipper.h
@@ -6,7 +6,7 @@
 
 class PolygonClipper {
 public:
-	void Clip(const Polygon2D& inputPolygon, Polygon2D& newPolygon, const Edge& clipWindowEdge);
+	Polygon2D Clip(const Polygon2D& inputPolygon, const Edge& clipWindowEdge);
 	float GetEdgeFunctionDet(const Edge& clipWindowEdge, const Vec2& currVertex);
 	bool VertexInside(const Edge& clipWindowEdge, const Vec2& currVertex);
 	Vec2 FindIntercept(const Edge& clipWindowEdge, const Edge& triEdge);
