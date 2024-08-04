764e0701c31937c0a337fed08845e9eb603dfb54
RyanByers1023 <ryanbyers1023@gmail.com>
2024-08-03 21:08:18 -0500

    Remove event attribute. Add initializer list constructor to preset input flags to false.

--- Project1/InputHandler.h
+++ Project1/InputHandler.h
@@ -3,29 +3,38 @@
 
 class InputHandler {
 public:
+	//default constructor:
+	InputHandler() : leftInput(false), rightInput(false), upInput(false), downInput(false), wInput(false), aInput(false), sInput(false), dInput(false), zInput(false), xInput(false), qInput(false), eInput(false), minusInput(false), plusInput(false) {};
+
 	//public methods:
 	void CheckForInput();
-	//input flags:
-	bool leftInput = false;
-	bool rightInput = false;
-	bool upInput = false;
-	bool downInput = false;
 
-	bool wInput = false;
-	bool aInput = false;
-	bool sInput = false;
-	bool dInput = false;
-	bool zInput = false;
-	bool xInput = false;
-	bool qInput = false;
-	bool eInput = false;
-	bool minusInput = false;
-	bool plusInput = false;
+	//input flags:
+
+	//arrow keys:
+	bool leftInput;
+	bool rightInput;
+	bool upInput;
+	bool downInput;
+
+	//wasd keys:
+	bool wInput;
+	bool aInput;
+	bool sInput;
+	bool dInput;
+
+	//extra controls:
+	bool zInput;
+	bool xInput;
+	bool qInput;
+	bool eInput;
+	bool minusInput;
+	bool plusInput;
+
+	//note: not getting specific in comments regarding keys' functionalities here b/c they most likely will change thorughout development
 
 private:
-	//private attributes:
-	SDL_Event event;
 	//private methods:
-	void HandleKeyDown(SDL_Event event);
-	void HandleKeyUp(SDL_Event event);
+	void HandleKeyDown(const SDL_Event& event);
+	void HandleKeyUp(const SDL_Event& event);
 };
