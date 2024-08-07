d030cdc3f931a69cf82c867e1e775e01ae687247
RyanByers1023 <ryanbyers1023@gmail.com>
2024-08-07 15:31:20 -0500

    Minor: add comments regarding shading algo steps

--- Project1/Shader.cpp
+++ Project1/Shader.cpp
@@ -4,6 +4,13 @@
 //could implement a more advanced shading algo, but i personally dont think it is worth it
 //wiki: https://en.wikipedia.org/wiki/Gouraud_shading
 
+//process:
+//1) Get face normals (should already have this)
+//2) Get vertex normals (average of face normals of all adjacent faces to vertex)
+//3) Calculate light intensity relative to ambient and directional light of light sources at vertex normals
+//4) draw line within screen-space 2-D poly mesh and --- choose: (linearly) / (non-linearly) interpolate between light intensities using a shaded line to fill polygon
+//5) repeat step 4 until face is shaded
+
 //note: this algorithm benefits visually from an increased poly count -- Diminishing returns as poly count increases past a certain point
 
 Shader::Shader(Screen* screenPtr) {
