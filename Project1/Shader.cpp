f1142d4ef03ee1cdd7c509508db443eeaeed113f
RyanByers1023 <ryanbyers1023@gmail.com>
2024-08-07 14:57:52 -0500

    Minor: create comments regarding gouraud shading

--- Project1/Shader.cpp
+++ Project1/Shader.cpp
@@ -1,4 +1,10 @@
 #include "Shader.h"
+//want to implement "gouraud" shading.
+//pretty antiquated: paper was written by gouraud in 1971, but visuals are acceptable for this project imo
+//could implement a more advanced shading algo, but i personally dont think it is worth it
+//wiki: https://en.wikipedia.org/wiki/Gouraud_shading
+
+//note: this algorithm benefits visually from an increased poly count -- Diminishing returns as poly count increases past a certain point
 
 Shader::Shader(Screen* screenPtr) {
 	this->screenPtr = screenPtr;
