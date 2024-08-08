235d021dfbe218da7ec1bc20bf856effee3b3dda
RyanByers1023 <ryanbyers1023@gmail.com>
2024-08-08 15:34:00 -0500

    fix: intializer list constructors in line

--- Project1/Line.h
+++ Project1/Line.h
@@ -7,14 +7,16 @@
 class Line{
 public:
 	//if no coordinates are given, set all to origin
-	Line(Screen* screenPtr) : screenPtr(screenPtr), line.v1.x(0.0f), line.v1.y(0.0f), line.v2.x(0.0f), line.v2.y(0.0f) {};
+	Line(Screen* screenPtr) : screenPtr(screenPtr), line(Vec2(0.0f, 0.0f), Vec2(0.0f, 0.0f)) {}
 	//floats given, store as edge
-	Line(Screen* screenPtr, const float& x1, const float& y1, const float& x2, const float& y2) : screenPtr(screenPtr), line.v1.x(x1), line.v1.y(y1), line.v2.x(x2), line..v2.y(y2) {};
+	Line(Screen* screenPtr, const float& x1, const float& y1, const float& x2, const float& y2) : screenPtr(screenPtr), line(Vec2(x1, y1), Vec2(x2, y2)) {}
 	//2D vertices (Vec2) given, store as edge
-	Line(Screen* screenPtr, const Vec2& v1, const Vec2& v2) : screenPtr(screenPtr), line.v1(v1), line.v2(v2) {};
+	Line(Screen* screenPtr, const Vec2& v1, const Vec2& v2) : screenPtr(screenPtr), line(v1, v2) {}
 	//edge given, store as is
 	Line(Screen* screenPtr, const Edge& line) : screenPtr(screenPtr), line(line) {};
+
 	void Draw();
+
 	Edge line;
 private:	
 	Screen* screenPtr;
