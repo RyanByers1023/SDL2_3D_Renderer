307d8fd5f9d01800df06ade6630faeb0c73658d8
RyanByers1023 <ryanbyers1023@gmail.com>
2024-08-08 15:32:44 -0500

    feat: Material store info regarding a single material for a mesh. Interacts with Light to shade objects

---
+++ Project1/Material.h
@@ -0,0 +1,25 @@
+#pragma once
+#include "Vec3.h"
+class Material {
+public:
+	//RGB value: base color under ambient light
+	Vec3 ambient;
+
+	//RGB value: base color under diffuse light
+	Vec3 diffuse;
+
+	//RGB value: color of specular highlights
+	Vec3 specular;
+
+	//controls size and intensity of specular highlights
+	float shininess;
+
+	//constructor:
+	Material(const Vec3& ambient, const Vec3& diffuse, const Vec3& specular, const float& shininess)
+		: ambient(ambient), diffuse(diffuse), specular(specular), shininess(shininess) {}
+
+	//clamps all RGB values between 0.0 and 1.0
+	void ClampColors();
+private:
+	Vec3 Clamp(const Vec3& color);
+};
