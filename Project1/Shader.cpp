ff6b4a5c25069b1fc3f195d1dd4482896dda8cd3
RyanByers1023 <ryanbyers1023@gmail.com>
2024-08-08 10:03:53 -0500

    feat: add bounding box code from prev shading method back for gouraud shading process

--- Project1/Shader.cpp
+++ Project1/Shader.cpp
@@ -21,3 +21,40 @@ void Shader::ShadePolygon(const Polygon2D& polygon) {
     
 }
 
+//bounding box:
+
+BoundingBox Shader::GetBoundingBox(const Polygon2D& polygon) {
+    BoundingBox boundingBox;
+
+    //calculate the minimum x coordinate (closest to origin) and minimum y coordinate
+    boundingBox.minPoint.x = std::min({ polygon.vertices[0].x, polygon.vertices[1].x, polygon.vertices[2].x });
+    boundingBox.minPoint.y = std::min({ polygon.vertices[0].y, polygon.vertices[1].y, polygon.vertices[2].y });
+    boundingBox.maxPoint.x = std::max({ polygon.vertices[0].x, polygon.vertices[1].x, polygon.vertices[2].x });
+    boundingBox.maxPoint.y = std::max({ polygon.vertices[0].y, polygon.vertices[1].y, polygon.vertices[2].y });
+
+    //now we need to clamp the bounding box to the screen coordinates
+    ClampBoundingBox(boundingBox);
+
+    return boundingBox;
+}
+
+void Shader::ClampBoundingBox(BoundingBox& boundingBox) {//process that clamps the boundingBox to be within the screen space, and convert floats to integers for easy iteration through the bounding box
+    boundingBox.minPoint.x = std::max({ 0, static_cast<int>(std::floor(boundingBox.minPoint.x)) }); //clamp between 0 and the min x val of the boundingBox (if a float, it is rounded down)
+    boundingBox.minPoint.y = std::max({ 0, static_cast<int>(std::floor(boundingBox.minPoint.y)) }); //clamp between 0 and the min y val of the boundingBox (if a float, it is rounded down)
+    boundingBox.maxPoint.x = std::min({ screenPtr->width - 1, static_cast<int>(std::ceil(boundingBox.maxPoint.x)) }); //clamp between screenWidth - 1 and the max x val of the boundingBox (if a float, it is rounded up)
+    boundingBox.maxPoint.y = std::min({ screenPtr->height - 1, static_cast<int>(std::ceil(boundingBox.maxPoint.y)) }); //clamp between screenHeight - 1 and the max y val of the boundingBox (if a float, it is rounded up)
+}
+
+//check for intersections:
+
+bool Shader::IntersectsTriangle(const Vec2& currPoint, const Polygon2D& polygon) { //if all of the below statements equate to true (all cross product operations >= 0) the point intersects polygon, therfore we want to draw this point.
+    return GetEdgeFunctionDet(currPoint, polygon.vertices[0], polygon.vertices[1]) >= 0 && //edge from v0 to v1
+        GetEdgeFunctionDet(currPoint, polygon.vertices[1], polygon.vertices[2]) >= 0 && //edge from v1 to v2
+        GetEdgeFunctionDet(currPoint, polygon.vertices[2], polygon.vertices[0]) >= 0;   //edge from v2 to v0
+}
+
+float Shader::GetEdgeFunctionDet(const Vec2& currPoint, const Vec2& v0, const Vec2& v1) { //Computes cross product between edge v0 -> v1 with respect to currPoint
+    float edgeFunctionValue = (v1.x - v0.x) * (currPoint.y - v0.y) - (v1.y - v0.y) * (currPoint.x - v0.x);
+    return edgeFunctionValue; //return value for above evaluation in IntersectsTriangle
+}
+
