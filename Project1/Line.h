d2156605d4ccd3dee81f177b083388dd61c0bada
RyanByers1023 <ryanbyers1023@gmail.com>
2024-08-10 14:19:45 -0500

    fix: remove copy of pointer to screenPtr as attribute. will be passed as parameter when needed. Replace raw pointers with smart pointers

--- Project1/Line.h
+++ Project1/Line.h
@@ -7,17 +7,20 @@
 class Line{
 public:
 	//if no coordinates are given, set all to origin
-	Line(Screen* screenPtr) : screenPtr(screenPtr), line(Vec2(0.0f, 0.0f), Vec2(0.0f, 0.0f)) {}
+	Line() : line(Vec2(0.0f, 0.0f), Vec2(0.0f, 0.0f)) {}
+
 	//floats given, store as edge
-	Line(Screen* screenPtr, const float& x1, const float& y1, const float& x2, const float& y2) : screenPtr(screenPtr), line(Vec2(x1, y1), Vec2(x2, y2)) {}
+	Line(const float& x1, const float& y1, const float& x2, const float& y2) : line(Vec2(x1, y1), Vec2(x2, y2)) {}
+
 	//2D vertices (Vec2) given, store as edge
-	Line(Screen* screenPtr, const Vec2& v1, const Vec2& v2) : screenPtr(screenPtr), line(v1, v2) {}
+	Line(const Vec2& v1, const Vec2& v2) : line(v1, v2) {}
+
 	//edge given, store as is
-	Line(Screen* screenPtr, const Edge& line) : screenPtr(screenPtr), line(line) {};
+	Line(const Edge& edge) : line(edge) {}
 
-	void Draw();
+	//draws edge stored within line
+	void Draw(std::shared_ptr<Screen>& screenPtr);
 
+	//the line
 	Edge line;
-private:	
-	Screen* screenPtr;
 };
