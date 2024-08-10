ea6b87333a4b1e68b01bcf50aa94b14b7b10c9dd
RyanByers1023 <ryanbyers1023@gmail.com>
2024-08-10 10:44:13 -0500

    feat: primitiveObjects store name as a string

--- Project1/PrimitiveObject.h
+++ Project1/PrimitiveObject.h
@@ -11,6 +11,7 @@ public:
 	std::vector<Vec3> vertices; //stores all of the vertices (corner points) of the object. Helpful in centeroid calculation. Mesh is formed from this vector
 	Vec3 position; //Stores the north face, bottom left vertice. (first vertex in mesh)
 	Vec3 centeroid; //center coordinate of the object.
+	string name; //for identification within worldObjects
 	//public methods:
 	void CalcCenteroid(); //adds all vertices together and divides by number of vertices. Centeroid stored as a Vec3
 };
