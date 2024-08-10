c372425268031b601ba81dbf0d52466f73cec4d7
RyanByers1023 <ryanbyers1023@gmail.com>
2024-08-10 09:36:16 -0500

    fix: Spawner now a class. Uses smart pointers instead of raw pointers

--- Project1/Spawner.cpp
+++ Project1/Spawner.cpp
@@ -1,7 +1,8 @@
 #include "Spawner.h"
 
-void SpawnCube(WorldObjects* worldObjectsPtr, std::string name, Vec3 position, int size) {
-	Cube cube(position, size); //create a 100px x 100px x 100px cube in the center of the screen. parameters: (Screen object, position to spawn object (Vec3), size of cube (int in pixels)
+void Spawner::SpawnCube(std::shared_ptr<WorldObjects> worldObjectsPtr, const std::string& name, const Vec3& position, const int& size) {
+	//create a 100px x 100px x 100px cube in the center of the screen (as long as position and size parameters haven't been passed)
+	Cube cube(position, size); 
 	auto it = worldObjectsPtr->objects.find(name);
 	if (it != worldObjectsPtr->objects.end()) {
 		std::cout << "There already exists an object with that name. Please name the object you wish to create something different.\n\n" << std::endl;
--- Project1/Spawner.h
+++ Project1/Spawner.h
@@ -3,6 +3,11 @@
 #include "WorldObjects.h"
 #include <string>
 #include <iostream>
+#include <memory>
 
-void SpawnCube(WorldObjects* worldObjectsPtr, std::string name, Vec3 position = { 40.0f, 20.0f, 180.0f }, int size = 100);
+class Spawner{
+public:
+    void SpawnCube(std::shared_ptr<WorldObjects> worldObjectsPtr, const std::string& name, const Vec3& position = { 40.0f, 20.0f, 180.0f }, const int& size = 100);
+}
+
 	
