0cc66618f6e45088869bbdfe5c52ff79d880dd90
RyanByers1023 <ryanbyers1023@gmail.com>
2024-08-10 10:44:34 -0500

    fix: add name to parameter when constructing cube

--- Project1/Spawner.cpp
+++ Project1/Spawner.cpp
@@ -2,7 +2,7 @@
 
 void Spawner::SpawnCube(std::shared_ptr<WorldObjects> worldObjectsPtr, const std::string& name, const Vec3& position, const int& size) {
 	//create a 100px x 100px x 100px cube in the center of the screen (as long as position and size parameters haven't been passed)
-	Cube cube(position, size); 
+	Cube cube(position, size, name); 
 	auto it = worldObjectsPtr->objects.find(name);
 	if (it != worldObjectsPtr->objects.end()) {
 		std::cout << "There already exists an object with that name. Please name the object you wish to create something different.\n\n" << std::endl;
