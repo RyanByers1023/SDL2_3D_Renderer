1053f89e906de9d35919b5a13e312c58247a8fa5
Ryan Byers <ryanbyers1023@gmail.com>
2024-08-12 19:23:18 -0500

    fix: worldObjectsPtr now a unique_ptr. when creating a cube object, the memory is manually reserved, cube is stored a raw pointer

--- Project1/Spawner.cpp
+++ Project1/Spawner.cpp
@@ -1,11 +1,11 @@
 #include "Spawner.h"
 
-void SpawnCube(WorldObjects* worldObjectsPtr, std::string name, Vec3 position, int size) {
-	Cube cube(position, size); //create a 100px x 100px x 100px cube in the center of the screen. parameters: (Screen object, position to spawn object (Vec3), size of cube (int in pixels)
+void SpawnCube(std::unique_ptr<WorldObjects>& worldObjectsPtr, std::string name, Vec3 position, int size) {
+	Cube* cube = new Cube(name, position, size); //create a 100px x 100px x 100px cube in the center of the screen. parameters: (Screen object, position to spawn object (Vec3), size of cube (int in pixels)
 	auto it = worldObjectsPtr->objects.find(name);
 	if (it != worldObjectsPtr->objects.end()) {
 		std::cout << "There already exists an object with that name. Please name the object you wish to create something different.\n\n" << std::endl;
 		return;
 	}
-	worldObjectsPtr->objects[name] = cube;
+	worldObjectsPtr->objects.emplace(name, cube);
 }
