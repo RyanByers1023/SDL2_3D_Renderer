46837b992ae33067842814a72004526693617c32
Ryan Byers <ryanbyers1023@gmail.com>
2024-08-12 22:13:54 -0500

    feat: Spawner now a class. Will be expanded further later.

--- Project1/Spawner.h
+++ Project1/Spawner.h
@@ -3,6 +3,11 @@
 #include "WorldObjects.h"
 #include <string>
 #include <iostream>
+#include <memory>
 
-void SpawnCube(std::unique_ptr<WorldObjects>& worldObjectsPtr, std::string name, Vec3 position = { 40.0f, 20.0f, 180.0f }, int size = 100);
+class Spawner {
+public:
+	void SpawnCube(const std::unique_ptr<WorldObjects>& worldObjectsPtr, const std::string& name, const Vec3& position = { 40.0f, 20.0f, 180.0f }, int size = 100);
+};
+
 	
