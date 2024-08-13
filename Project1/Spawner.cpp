0a6e2302632466aa9cd17b27066fa7a768af7805
Ryan Byers <ryanbyers1023@gmail.com>
2024-08-13 18:41:32 -0500

    minor: cube intialized with shared_ptr. mark methods as const where needed

--- Project1/Spawner.cpp
+++ Project1/Spawner.cpp
@@ -1,7 +1,7 @@
 #include "Spawner.h"
 
-void  Spawner::SpawnCube(const std::unique_ptr<WorldObjects>& worldObjectsPtr, const std::string& name, const Vec3& position, int size) {
-	Cube* cube = new Cube(name, position, size); //create a 100px x 100px x 100px cube in the center of the screen. parameters: (Screen object, position to spawn object (Vec3), size of cube (int in pixels)
+void  Spawner::SpawnCube(const std::unique_ptr<WorldObjects>& worldObjectsPtr, const std::string& name, const Vec3& position, int size) const{
+	auto cube = std::make_shared<Cube>(name, position, size); //create a 100px x 100px x 100px cube in the center of the screen. parameters: (Screen object, position to spawn object (Vec3), size of cube (int in pixels)
 	auto it = worldObjectsPtr->objects.find(name);
 	if (it != worldObjectsPtr->objects.end()) {
 		std::cout << "There already exists an object with that name. Please name the object you wish to create something different.\n\n" << std::endl;
--- Project1/Spawner.h
+++ Project1/Spawner.h
@@ -7,7 +7,7 @@
 
 class Spawner {
 public:
-	void SpawnCube(const std::unique_ptr<WorldObjects>& worldObjectsPtr, const std::string& name, const Vec3& position = { 40.0f, 20.0f, 180.0f }, int size = 100);
+	void SpawnCube(const std::unique_ptr<WorldObjects>& worldObjectsPtr, const std::string& name, const Vec3& position = { 40.0f, 20.0f, 180.0f }, int size = 100) const;
 };
 
 	
