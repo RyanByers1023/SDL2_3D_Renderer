c457c853a0319b9c25d77e387762e4032bb30727
RyanByers1023 <ryanbyers1023@gmail.com>
2024-08-18 17:12:03 -0500

    minor: add space after Material::Clamp()

--- Project1/Material.cpp
+++ Project1/Material.cpp
@@ -6,7 +6,7 @@ void Material::ClampColors() {
     specular = Clamp(specular);
 }
 
-Vec3 Material::Clamp(const Vec3& color) {
+Vec3 Material::Clamp(const Vec3& color){
     return Vec3(std::clamp(color.x, 0.0f, 1.0f),
                 std::clamp(color.y, 0.0f, 1.0f),
                 std::clamp(color.z, 0.0f, 1.0f));
