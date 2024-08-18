59173058099e1c5fa0999c45d6363a9727f1b0fe
RyanByers1023 <ryanbyers1023@gmail.com>
2024-08-18 17:56:33 -0500

    fix: change declaration of ClampBoundingBox to match defintion, takes in screenPtr

--- Project1/Shader.h
+++ Project1/Shader.h
@@ -7,6 +7,7 @@
 #include <map>
 #include <cmath>
 #include <algorithm>
+#include <memory>
 
 struct BoundingBox { //used for determing the area we need to check for intersections
 	Vec2 minPoint; // -- > top-left corner of bounding box
@@ -21,7 +22,7 @@ public:
 private:
 	Vec3 ClampColor(const Vec3& color);
 	BoundingBox GetBoundingBox(const Polygon2D& polygon);
-	void ClampBoundingBox(BoundingBox& boundingBox);
+	void ClampBoundingBox(std::unique_ptr<Screen> screenPtr, BoundingBox& boundingBox);
 	bool IntersectsPolygon(const Vec2& currPoint, const Polygon2D& polygon);
 	float GetEdgeFunctionDet(const Vec2& currPoint, const Vec2& v0, const Vec2& v1);
 	Vec3 ClampColor(const Vec3& color);
