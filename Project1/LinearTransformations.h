80b3909442ffc4ac15feb8130bcf77ce46df40ad
RyanByers1023 <ryanbyers1023@gmail.com>
2024-08-18 17:11:38 -0500

    minor: mark functions as const where necessary

--- Project1/LinearTransformations.h
+++ Project1/LinearTransformations.h
@@ -7,6 +7,6 @@
 
 class LinearTransformations {
 public:
-	void ApplyTransformation(const std::unique_ptr<InputHandler>& inputHandlerPtr, const std::unique_ptr<Time>& timePtr, std::shared_ptr<PrimitiveObject> object) const;
-	void ApplyRotation(const std::unique_ptr<InputHandler>& inputHandlerPtr, const std::unique_ptr<Time>& timePtr, std::shared_ptr<PrimitiveObject> object) const;
+	void ApplyTransformation(const std::unique_ptr<InputHandler>& inputHandlerPtr, const std::unique_ptr<Time>& timePtr, std::unique_ptr<PrimitiveObject>& object) const;
+	void ApplyRotation(const std::unique_ptr<InputHandler>& inputHandlerPtr, const std::unique_ptr<Time>& timePtr, std::unique_ptr<PrimitiveObject>& object) const;
 };
